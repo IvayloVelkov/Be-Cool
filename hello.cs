23afec0141a182adff35af158802392a090969d8
IvayloVelkov <ivchovelkov@gmail.com>
2016-05-18 11:09:26 +0300

    Better

--- hello.cs
+++ hello.cs
@@ -12,8 +12,8 @@ namespace Trapezoid_Area
         {
             var b1 = double.Parse(Console.ReadLine());
             var b2 = double.Parse(Console.ReadLine());
-            var h = double.Parse(Console.ReadLine());
-            var area = (b1 + b2) * h / 2;
+            var H = double.Parse(Console.ReadLine());
+            var area = (b1 + b2) * H / 2;
 
             Console.WriteLine("Trapezoid area = {0}", area);
         }
